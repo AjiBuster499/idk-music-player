a6522e5804ed92a51a0d78bf359d719a8d98ce91
S. M <sbug1442@gmail.com>
2020-12-12 20:13:10 -0500

    added switching to UI

--- src/main/java/com/ajibuster/app/App.java
+++ src/main/java/com/ajibuster/app/App.java
@@ -7,8 +7,11 @@ import java.util.Scanner;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -17,7 +20,8 @@ import javafx.stage.Stage;
  */
 public class App extends Application {
   // Global Items
-  Button play, closeWindow, pause, newSong; // TODO: newSong is for debugging, remove later
+  Button play, closeWindow, pause, switchSong;
+  TextField songField;
   Stage window;
   MediaHandler mh;
   Scanner scanner = new Scanner(System.in);
@@ -35,14 +39,20 @@ public class App extends Application {
     mh = new MediaHandler(filePath);
 
     HBox topMenu = new HBox();
+    HBox centerMenu = new HBox();
     BorderPane bPane = new BorderPane();
     bPane.setTop(topMenu);
+    bPane.setCenter(centerMenu);
+    centerMenu.setSpacing(10);
 
     // Button Creation
     closeWindow = new Button("Close Window");
     play = new Button("Play");
     pause = new Button("Pause");
-    newSong = new Button("DEBUGGER: NEW SONG");
+    switchSong = new Button("Select Song");
+
+    // Text Field Creation
+    songField = new TextField();
 
     // Close Windows
     window.setOnCloseRequest(e -> closeProgram());
@@ -51,11 +61,12 @@ public class App extends Application {
     // Button Actions
     play.setOnAction(e -> mh.playMusic());
     pause.setOnAction(e -> mh.pauseMusic());
-    newSong.setOnAction(e -> {
-      this.mh = MediaHandler.switchSong("JumpingFish.mp3");
+    switchSong.setOnAction(e -> {
+      mh = MediaHandler.switchSong(songField.getText());
     });
 
-    topMenu.getChildren().addAll(play, pause, newSong, closeWindow);
+    topMenu.getChildren().addAll(play, pause, closeWindow);
+    centerMenu.getChildren().addAll(songField, switchSong);
 
     Scene scene = new Scene(bPane, 300, 250);
     window.setScene(scene);
