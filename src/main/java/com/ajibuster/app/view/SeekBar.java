2188128014eff3475013a0f0f147b9e451af985c
S. M <sbug1442@gmail.com>
2021-01-28 15:58:22 -0500

    Starting SeekBar.

---
+++ src/main/java/com/ajibuster/app/view/SeekBar.java
@@ -0,0 +1,12 @@
+package com.ajibuster.app.view;
+
+import com.ajibuster.app.eventbus.EventBus;
+
+import javafx.scene.control.Slider;
+
+public class SeekBar extends Slider {
+
+  public SeekBar (EventBus eventBus) {
+  }
+  
+}
